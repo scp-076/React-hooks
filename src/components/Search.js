40151c95520f40306ca8a100878abbf4d6e07f89
Михаил Евсюнин <evsunin@ittest-team.ru>
2020-04-06 18:57:04 +0300

    init commit

---
+++ src/components/Search.js
@@ -0,0 +1,36 @@
+import React, {useContext, useState} from 'react';
+import {AlertContext} from "../context/alert/alertContext";
+import {GithubContext} from "../context/github/GithubContext";
+
+export const Search = () => {
+    const [value, setValue] = useState('');
+    const alert = useContext(AlertContext);
+    const github = useContext(GithubContext);
+    const onSubmit = (event) => {
+        if(event.key !== 'Enter') {
+            return
+        }
+
+        github.clearUsers();
+
+        if(value.trim()) {
+            alert.hide();
+            github.search(value.trim());
+        } else {
+            alert.show('Enter user data')
+        }
+    };
+
+    return(
+        <div className="form-group">
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Enter user's nickname.."
+                onKeyPress={onSubmit}
+                value={value}
+                onChange={event => setValue(event.target.value)}
+            />
+        </div>
+    )
+};
---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React, {useContext} from 'react';
+import {Search} from "../components/Search";
+import {Card} from "../components/Card";
+import {GithubContext} from "../context/github/GithubContext";
+
+export const Home = () => {
+    const {loading, users} = useContext(GithubContext);
+
+    return(
+        <React.Fragment>
+            <Search />
+            <div className="row">
+                {loading
+                 ? <p className="text-center">Loading..</p>
+                 :  users.map(user => (
+                            <div className="col-sm-4 mb-4" key={user.id}>
+                                <Card user={user} />
+                            </div>
+                    ))
+                }
+            </div>
+        </React.Fragment>
+    )
+};
